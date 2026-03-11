08db51345c3da0bb7fa0e10cfd752d355cf378c7
TJ25-cool <tjalloh325@gmail.com>
2026-03-11 10:00:04 -0400

    Add a simple question and answer logic

---
+++ deep-thought/deep.py
@@ -0,0 +1,5 @@
+question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if question == "42" or question =="Forty Two" or question == "forty-two":
+    print("Yes")
+else:
+    print("No")
